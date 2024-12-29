a55a4c5f9bada48b06701f69a3ee7288c06c1d21
Eugenio Estrada <eugenio.estrada@gmail.com>
2024-12-29 15:41:32 +0100

    Add task deletion functionality and unique ID generation for tasks

--- src/Model/Task.cs
+++ src/Model/Task.cs
@@ -2,6 +2,7 @@ namespace ToDo.Model;
 
 public class ToDoTask
 {
+    public string Id { get; } = Guid.NewGuid().ToString();
     public DateTime CreationTime { get; set; }
     public DateTime DueTime { get; set; }
     public string Title { get; set; }
